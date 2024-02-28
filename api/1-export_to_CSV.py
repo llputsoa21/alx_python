4f326cb2b17f834c3e5a87dc0734fed5eb7d7381
Lesego <llputsoa@gmail.com>
2024-02-28 18:36:58 +0200

    export to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,3 +1,13 @@
+""" extend Python script to export data in the CSV format.
+
+Requirements:
+
+Records all tasks that are owned by this employee
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv """
+
+
+
 import csv
 import requests
 import sys
